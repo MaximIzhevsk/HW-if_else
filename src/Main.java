d12f14f5f44b875766aa55f93d2d6bbe554f5bb4
grafe <maximizhevsk@gmail.com>
2024-10-07 16:34:04 +0400

    задача 1 выполнена

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+
+        //задание 1
+
+        int age = 18;
+        String message;
+        boolean isAdult = age >= 18;
+        if(isAdult){
+            message = "он совершеннолетний";
+        } else {
+            message = "он не достиг совершеннолетия, нужно немного подождать";
+        }
+        System.out.println("Если возраст человека равен " + age + ", то " + message);
+        System.out.println();
+
+        //задание 2
+
+
     }
 }
