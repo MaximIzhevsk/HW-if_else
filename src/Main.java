b839fa1bc8a4abe39342708e350c97a57e59e7f7
grafe <maximizhevsk@gmail.com>
2024-10-07 17:09:35 +0400

    задача 4 выполнена

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,9 @@ public class Main {
         String message;
         boolean isAdult = age >= 18;
         if (isAdult) {
-            message = "он совершеннолетний";
+            message = "он совершеннолетний.";
         } else {
-            message = "он не достиг совершеннолетия, нужно немного подождать";
+            message = "он не достиг совершеннолетия, нужно немного подождать.";
         }
         System.out.println("Если возраст человека равен " + age + ", то " + message);
         System.out.println();
@@ -18,9 +18,9 @@ public class Main {
 
         int temperature = 4;
         if (temperature < 5) {
-            System.out.println("На улице " + temperature + " градусов, нужно надеть шапку");
+            System.out.println("На улице " + temperature + " градусов, нужно надеть шапку.");
         } else {
-            System.out.println("На улице " + temperature + " градусов, можно идти без шапки");
+            System.out.println("На улице " + temperature + " градусов, можно идти без шапки.");
         }
         System.out.println();
 
@@ -29,10 +29,28 @@ public class Main {
         int speed = 60;
         boolean isSpeedingFine = speed > 60;
         if (isSpeedingFine){
-            System.out.println("Если скорость " + speed + ", то придется заплатить штраф");
+            System.out.println("Если скорость " + speed + ", то придется заплатить штраф.");
         } else {
-            System.out.println("Если скорость " + speed + ", можно ездить спокойно");
+            System.out.println("Если скорость " + speed + ", можно ездить спокойно.");
         }
+        System.out.println();
+
+        //задание 4
+
+        int yearsOld = 1;
+        String messageYearsOld;
+        if (yearsOld >=2 && yearsOld <= 6) {
+            messageYearsOld = "ему нужно ходить в детский сад.";
+        } else if (yearsOld >= 7 && yearsOld <= 17) {
+            messageYearsOld = "ему нужно ходить в школу.";
+        } else if (yearsOld >= 18 && yearsOld <= 24) {
+            messageYearsOld = "его место в университете.";
+        } else if (yearsOld > 24) {
+            messageYearsOld = "ему пора ходить на работу.";
+        } else {
+            messageYearsOld = "ему пока можно никуда не ходить.";
+        }
+        System.out.println("Если возраст человека равен " + yearsOld + ", то " + messageYearsOld);
 
     }
 }
